c5530895521d36d01bd14af22bb5511c202e9167
Evgeniy <Jack-inc@yandex.ru>
2024-02-10 10:31:35 +0400

    решение

--- seminar10.py
+++ seminar10.py
@@ -5,6 +5,11 @@ import random
 lst = ['robot'] * 10 + ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI': lst})
+unique_val = data['whoAmI'].unique()
+
+
+for val in unique_val:
+    data[val] = (data['whoAmI'] == val).astype(int) #true = 1 false = 0
+data = data.drop('whoAmI', axis=1)
+
 print(data)
-
-
